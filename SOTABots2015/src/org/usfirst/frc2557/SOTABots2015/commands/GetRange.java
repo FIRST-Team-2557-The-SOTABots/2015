f955a7ee61ea8ff15e70fd4219fca8f44f2f1a03
VaneRaklan <antoniowii@yahoo.com>
2015-01-22 12:44:40 -0800

    Added in some sensors that we will be using soon. They are now ready to be used in commands and subsystems.

--- SOTABots2015/src/org/usfirst/frc2557/SOTABots2015/commands/GetRange.java
+++ SOTABots2015/src/org/usfirst/frc2557/SOTABots2015/commands/GetRange.java
@@ -22,7 +22,7 @@ public class GetRange extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	System.out.println(RobotMap.rangeBack1.getRangeInches());
+    	System.out.println(RobotMap.rangeFrontLeft.getAverageVoltage());
     	
     }
 
