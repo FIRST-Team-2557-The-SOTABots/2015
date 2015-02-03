92d2de1bdc419acbcdea762d850ab858f2e89a90
Adam-member <adamsorg@icloud.com>
2015-02-02 17:52:08 -0800

    no message

--- SOTABots2015/src/org/usfirst/frc2557/SOTABots2015/subsystems/AccelerometerSub.java
+++ SOTABots2015/src/org/usfirst/frc2557/SOTABots2015/subsystems/AccelerometerSub.java
@@ -12,11 +12,23 @@ public class AccelerometerSub extends Subsystem {
     
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-	BuiltInAccelerometer locationAccelerometer2 = RobotMap.locationAccelerometer2;
+	BuiltInAccelerometer locationAccelerometer = RobotMap.locationAccelerometer2;
 	
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public double getX(){
+    	return locationAccelerometer.getX();
+    }
+    
+    public double getY(){
+    	return locationAccelerometer.getY();
+    }
+    
+    public double getZ(){
+    	return locationAccelerometer.getZ();
     }
 }
 
