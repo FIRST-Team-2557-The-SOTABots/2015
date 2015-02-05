833e434bf5f2325c811dd91a4870ac15c9b1dcec
LightBoko <lightboko@gmail.com>
2015-02-04 17:24:43 -0800

    erdded der surbsursturm fer der smert dershberd

---
+++ SOTABots2015/src/org/usfirst/frc2557/SOTABots2015/subsystems/SmartDashboardSS.java
@@ -0,0 +1,43 @@
+package org.usfirst.frc2557.SOTABots2015.subsystems;
+
+import org.usfirst.frc2557.SOTABots2015.RobotMap;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ *
+ */
+public class SmartDashboardSS extends Subsystem {
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    public void smartDashboardOutputs() {
+    	//Limit Switch
+    	if(RobotMap.momentarySwitch.equals(true)){
+    		SmartDashboard.putString("Limit Switch", "ON");
+    	}
+    	else{
+    		SmartDashboard.putString("Limit Switch", "OFF");
+    	}
+    	//RangeFinders
+    	SmartDashboard.putNumber("The Front Left Range Finder is seeing", RobotMap.rangeFrontLeft.getVoltage()/.009765);
+		SmartDashboard.putNumber("The Right Range Finder is seeing", RobotMap.rangeFrontRight.getVoltage()/.009765);
+		SmartDashboard.putNumber("The Center Range Finder is seeing", RobotMap.rangeBack.getVoltage()/.009765);
+		SmartDashboard.putNumber("The Left Range Finder is seeing an average range of ", RobotMap.rangeFrontLeft.getAverageVoltage()/.009765);
+    	SmartDashboard.putNumber("The Right Range Finder is seeing an average range of", RobotMap.rangeFrontRight.getAverageVoltage()/.009765);
+		SmartDashboard.putNumber("The Center Range Finder is seeing an average range of", RobotMap.rangeBack.getAverageVoltage()/.009765);
+		//Gyro
+		SmartDashboard.putNumber("Gyro angle is: ",RobotMap.locationGyro.getAngle() );
+		//Accelerometers
+		SmartDashboard.putNumber("Accelerometer Y-Axis: ", RobotMap.locationAccelerometer2.getY());
+		SmartDashboard.putNumber("Accelerometer X-Axis: ", RobotMap.locationAccelerometer2.getX());
+    }
+}
+
