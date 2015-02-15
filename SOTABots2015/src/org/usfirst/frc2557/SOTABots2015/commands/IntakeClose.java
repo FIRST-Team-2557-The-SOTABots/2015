d344365ca7616217c0708b3c74f44399b0cc9073
Northern-Emperor <areichnorris@gmail.com>
2015-02-14 19:58:05 -0800

    Finished the intake open and close functions on the controllers.

--- SOTABots2015/src/org/usfirst/frc2557/SOTABots2015/commands/IntakeClose.java
+++ SOTABots2015/src/org/usfirst/frc2557/SOTABots2015/commands/IntakeClose.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc2557.SOTABots2015.commands;
 
+import org.usfirst.frc2557.SOTABots2015.Robot;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -10,10 +12,12 @@ public class IntakeClose extends Command {
     public IntakeClose() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+    	requires(Robot.pnuematics);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	Robot.pnuematics.intakeClose();
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -22,7 +26,7 @@ public class IntakeClose extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
--- SOTABots2015/src/org/usfirst/frc2557/SOTABots2015/subsystems/Pnuematics.java
+++ SOTABots2015/src/org/usfirst/frc2557/SOTABots2015/subsystems/Pnuematics.java
@@ -11,11 +11,11 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Pnuematics extends Subsystem {
     public static DoubleSolenoid grab = RobotMap.grab;		//Breakout 1; port 0 and 1
-    public static DoubleSolenoid intakeSol;					//Breakout 1; port 2 and 3
-    public static DoubleSolenoid ejector;					//Breakout 1; port 4 and 5
-    public static DoubleSolenoid rightHook;					//Breakout 1; port 6 and 7
-    public static DoubleSolenoid backHook;					//Breakout 2; port 0 and 1
-    public static DoubleSolenoid leftHook;					//Breakout 2; port 2 and 3
+    public static DoubleSolenoid intake = RobotMap.intakeSol;					//Breakout 1; port 2 and 3
+    public static DoubleSolenoid ejector = RobotMap.ejector;					//Breakout 1; port 4 and 5
+    public static DoubleSolenoid rightHook = RobotMap.rightHook;					//Breakout 1; port 6 and 7
+    public static DoubleSolenoid backHook = RobotMap.backHook;					//Breakout 2; port 0 and 1
+    public static DoubleSolenoid leftHook = RobotMap.leftHook;					//Breakout 2; port 2 and 3
     
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
@@ -24,8 +24,11 @@ public class Pnuematics extends Subsystem {
     public void grab(){
     	grab.set(Value.kForward);
     }
-    public void intake(){
-    	intakeSol.set(Value.kForward);
+    public void intakeClose(){
+    	intake.set(Value.kForward);
+    }
+    public void intakeOpen(){
+    	intake.set(Value.kReverse);
     }
     public void eject(){
     	ejector.set(Value.kForward);
